c098538494c3eea323515f087f5e6fa2b6e0dd67
Joabe Alves <joabealvesmorais@gmail.com>
2024-09-16 23:48:39 -0300

    tenta implementação de lógica do login

--- gamenest/frontend/src/pages/LoginPage.vue
+++ gamenest/frontend/src/pages/LoginPage.vue
@@ -1,10 +1,65 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { api } from '@/api'
+import { useUserStore } from '@/stores/userStore'
+// import { isAxiosError } from 'axios'
+// import { isApplicationError } from '@/composables/useApplicationError'
+// import type { ApplicationError } from '@/types'
+
+const identifier = ref('')
+const password = ref('')
+// const loading = ref(false)
+// const exception = ref<ApplicationError>()
+const router = useRouter()
+
+const userStore = useUserStore()
+
+async function authenticate() {
+  // try {
+  // loading.value = true
+  // exception.value = undefined
+  const { data } = await api.post('auth/local', {
+    identifier: identifier.value,
+    password: password.value
+  })
+  const { jwt } = data
+
+  const res = await api.get('users/me', {
+    headers: {
+      Authorization: `Bearer ${jwt}`
+    },
+    params: {
+      populate: 'role'
+    }
+  })
+
+  const role = res.data.role.type
+
+  userStore.authenticated(res.data, jwt)
+
+  if (role === 'admin') {
+    router.push('/admin')
+  } else {
+    router.push('/')
+  }
+  // } catch (e) {
+  //   if (isAxiosError(e) && isApplicationError(e.response?.data)) {
+  //     exception.value = e.response?.data
+  //   }
+  // } finally {
+  //   loading.value = false
+  // }
+}
+</script>
+
 <template>
   <div class="border-bottom fill"></div>
 
   <div class="caixa-login">
     <div class="container col col-md-6 col-xl-3">
       <h1 class="mb-4">GameNest</h1>
-      <form class="border p-3 rounded my-3">
+      <form @submit.prevent="authenticate" class="border p-3 rounded my-3">
         <h4>Entrar</h4>
         <div class="mb-3">
           <label for="exampleInputEmail1" class="form-label">Email</label>
@@ -13,12 +68,19 @@
             class="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            v-model="identifier"
             placeholder="exemplo@mail.com"
+            required
           />
         </div>
         <div class="mb-3">
           <label for="exampleInputPassword1" class="form-label">Senha</label>
-          <input type="password" class="form-control" id="exampleInputPassword1" />
+          <input 
+            type="password"
+            class="form-control"
+            v-model="password"
+            id="exampleInputPassword1" />
+            required
         </div>
         <button type="submit" class="btn btn-primary">Enviar</button>
       </form>
--- gamenest/frontend/src/types/index.ts
+++ gamenest/frontend/src/types/index.ts
@@ -1,13 +1,24 @@
 export type Game = {
-    id: number,
-    Nome: string,
-    Descricao: string,
-    Desenvolvedora: string,
-    Preco: number,
-    Capa: Cover
+  id: number
+  Nome: string
+  Descricao: string
+  Desenvolvedora: string
+  Preco: number
+  Capa: Cover
 }
 
 type Cover = {
-    id: number,
-    url: string
+  id: number
+  url: string
+}
+
+export type User = {
+  id: number
+  username: string
+  email: string
+  role: Role
+}
+
+type Role = {
+  name: string
 }
