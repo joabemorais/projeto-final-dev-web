4be6d76a0067dea6cba1fdf0abb86f373ade55cb
Joabe Alves <joabealvesmorais@gmail.com>
2024-09-22 17:55:54 -0300

    implementa pagina não encontrada

--- gamenest/frontend/src/router/index.ts
+++ gamenest/frontend/src/router/index.ts
@@ -9,6 +9,7 @@ import GamesAdmin from '@/pages/Admin/GamesAdmin.vue'
 import GameForm from '@/pages/Admin/GameForm.vue'
 import RegisterPage from '@/pages/RegisterPage.vue'
 import Library from '@/pages/Library.vue'
+import NotFound from '@/pages/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -75,6 +76,11 @@ const router = createRouter({
       meta: {
         requiresAuth: true
       }
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      name: 'notFound',
+      component: NotFound
     }
   ]
 })
